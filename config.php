a6f369526e35fbb2ec7308394e744b9fc504d849
YasMoh77 <hgq1100@yahoo.com>
2024-05-27 08:49:45 +0300

    ADD payment gate

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+    "mode" => "test",
+    "live" => array(
+        "baseUrl" => "https://checkout.kashier.io",
+        "apikey" => "",
+         "mid" => ""
+    ),
+    "test" => array(
+        "baseUrl" => "https://checkout.kashier.io",
+        "apikey" => "49c02cfa-8a4e-4120-8aa2-b154a6d08573",        
+        "mid" => "MID-3552-454"
+    )
+);
---
+++ index.php
@@ -0,0 +1,68 @@
+
+
+<?php
+
+include  'backend.php';
+
+?>
+
+<html>
+
+<head>
+<script>
+  function responseCallBack(e){ 
+  if(e.data.message== "success"){
+      console.log("Success payment", e.data )
+  }else if(e.data.message == "failure"){
+      console.log("Failure payment", e.data )
+  }else{
+      console.log("Other Actions", e.data )
+  }
+}
+     if (window.addEventListener) {
+       console.log("if");
+       addEventListener("message", responseCallBack, false)
+   } else {
+    console.log("else");     
+       attachEvent("onmessage", responseCallBack)
+   }
+</script>
+</head>​
+
+<body>
+
+<a target="_blank" href="<?php echo $hppUrl ?>"> Click to check Hosted Payment Page Form </a>
+
+<script id="kashier-iFrame"
+
+      src="<?php echo $order->baseUrl ?>/kashier-checkout.js"
+
+      data-amount="<?php echo $order->amount ?>"
+
+      data-description="some description"
+
+      data-hash="<?php echo $hash ?>"
+
+      data-currency="<?php echo $order->currency ?>"
+
+      data-orderId="<?php echo $order->merchantOrderId ?>"
+
+      data-merchantId="<?php echo $order->mid ?>"
+
+      data-allowedMethods="<?php echo $order->allowedMethods ?>"
+
+      data-merchantRedirect="http://localhost/Php-Checkout-Demo/iFrameCallback.php"
+
+      data-mode="<?php echo $order->mode ?>"
+
+      data-store = "Qassat"
+
+      data-type = "external"
+
+      data-display="en"  >
+ </script>
+
+</body>
+
+</html>
+
